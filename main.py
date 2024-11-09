1cf9d20be4ae821a6f5d2322a37b05b0ec558370
marbel <othmane.xx90@gmail.com>
2024-11-09 19:22:09 +0100

    is anagram

--- main.py
+++ main.py
@@ -9,4 +9,25 @@
                 dic[item] = None
         return False
         
-         
+ class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic = {}
+        for c_s in s:
+            if c_s in dic.keys():
+                dic[c_s] += 1
+            else:
+                dic[c_s] = 1
+        print(s)
+        print(dic)
+        for c_t in t:
+            if c_t in dic.keys():
+                dic[c_t] -= 1
+            else:
+                return False
+        print(dic)
+        for index in dic:
+            if dic[index] != 0:
+                return False
+        return True
+
+
